9c134d436af922a1eab0ba38fc5038c8ba041ca3
sanara <sanara@cpqd.com.br>
2025-09-08 10:28:28 -0300

    Adicionando métricas com Prometheus e Grafana '

--- Curso 2 - Java e Quarkus desenvolva aplicacoes Cloud Native/banking-service/src/main/java/com/sanara/agencias/service/AgenciaService.java
+++ Curso 2 - Java e Quarkus desenvolva aplicacoes Cloud Native/banking-service/src/main/java/com/sanara/agencias/service/AgenciaService.java
@@ -6,6 +6,7 @@ import com.sanara.agencias.domain.http.SituacaoCadastral;
 import com.sanara.agencias.exception.AgenciaNaoAtivaOuNaoEncontradaException;
 import com.sanara.agencias.repository.AgenciaRepository;
 import com.sanara.agencias.service.http.SituacaoCadastralHttpService;
+import io.micrometer.core.instrument.MeterRegistry;
 import io.quarkus.logging.Log;
 import jakarta.enterprise.context.ApplicationScoped;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
@@ -14,9 +15,11 @@ import org.eclipse.microprofile.rest.client.inject.RestClient;
 public class AgenciaService {
 
     private final AgenciaRepository agenciaRepository;
+    private final MeterRegistry meterRegistry;
 
-    AgenciaService(AgenciaRepository agenciaRepository) {
+    AgenciaService(AgenciaRepository agenciaRepository, MeterRegistry meterRegistry) {
         this.agenciaRepository = agenciaRepository;
+        this.meterRegistry = meterRegistry;
     }
 
     @RestClient
@@ -25,10 +28,14 @@ public class AgenciaService {
     public void cadastrar(Agencia agencia) {
         AgenciaHttp agenciaHttp = situacaoCadastralHttpService.buscarPorCnpj(agencia.getCnpj());
         if(agenciaHttp != null && agenciaHttp.getSituacaoCadastral().equals(SituacaoCadastral.ATIVO)) {
+            //adiciona métrica de agencia adicionada
+            this.meterRegistry.counter("agencia_adicionada_count").increment();
             Log.info("Agencia com CNPJ " + agencia.getCnpj() + " foi adicionada");
             agenciaRepository.persist(agencia);
         } else {
             Log.info("Agencia com CNPJ " + agencia.getCnpj() + " não ativa ou não encontrada");
+            //adiciona métrica de agencia nao adicionada
+            this.meterRegistry.counter("agencia_nao_adicionada_count").increment();
             throw new AgenciaNaoAtivaOuNaoEncontradaException();
         }
     }
